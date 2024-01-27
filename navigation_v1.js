0bc506b9ec10dd7283fcd97872c29d41331536b6
creamandclayco <155862437+creamandclayco@users.noreply.github.com>
2024-01-27 12:05:54 -0500

    Create navigation_v1.js

---
+++ navigation_v1.js
@@ -0,0 +1,26 @@
+let cc_nav = document.querySelector('.navbar');
+
+function updateNavHeightVar(){
+    let newHeight = cc_nav.offsetHeight + 'px';
+    document.documentElement.style.setProperty('--nav-height', newHeight);
+}
+
+if (document.querySelector('.navbar')){
+    updateNavHeightVar();
+}
+
+
+function debounce(func, wait) {
+    let timeout;
+    return function executedFunction(...args) {
+        const later = () => {
+            clearTimeout(timeout);
+            func(...args);
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+};
+
+
+window.addEventListener('resize', debounce(updateNavHeightVar, 200));
