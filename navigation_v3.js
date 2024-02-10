a8f44dea7202c5ce2a77b6f309c0337221e4f628
creamandclayco <155862437+creamandclayco@users.noreply.github.com>
2024-02-09 19:32:09 -0500

    Create navigation_v3.js

---
+++ navigation_v3.js
@@ -0,0 +1,40 @@
+(function() {
+
+
+let cc_nav = document.querySelector('.navbar');
+
+function updateNavHeightVar(){
+    let newHeight = cc_nav.offsetHeight + 'px';
+    document.documentElement.style.setProperty('--nav-height', newHeight);
+}
+
+if (document.querySelector('.navbar')){
+    updateNavHeightVar();
+}
+
+function debounce(func, wait) {
+    let timeout;
+    return function executedFunction(...args) {
+        const later = () => {
+            clearTimeout(timeout);
+            func(...args);
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+};
+
+window.addEventListener('resize', debounce(updateNavHeightVar, 200));
+
+
+
+if (cc_nav){
+    let menuButton = document.querySelector('.menu-button');
+    let body = document.querySelector('body');
+    menuButton.addEventListener('touchstart', function(){
+        body.classList.toggle('body--mobile-nav-open');
+    })
+}
+
+
+})();
